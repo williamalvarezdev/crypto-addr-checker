f6f9423e3404b3188e94a2e6f440ddb81a766580
William Alvarez <williamalvarezdev@gmail.com>
2025-05-08 00:47:44 +0100

    setting up github actions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Blockfrost = require("@blockfrost/blockfrost-js");
 const API = new Blockfrost.BlockFrostAPI({
-  projectId: "preprodx2V0nPCSUCQRitmZv9fUMCQu3BiVVXBg", 
+  projectId: env.PROJECTID, 
 });
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
